9c2fd707a11902ab566454b728f4593b245d67bb
mczhaozl <2575630942@qq.com>
2022-11-27 14:05:39 +0800

    [feat]增加ts 代码

--- tsIndex.ts
+++ tsIndex.ts
@@ -1,9 +1,28 @@
 const fs = require('fs-extra')
 const path = require('path')
-export default  function CreateLockCode(setting) {
-    const config = [formatSetting, formatPathMap].reduce((per, cur) => cur(per), setting)
+type Tconfig = {
+    // 'replace' 取代被篡改的代码 'manual' 仅报错需要手动替换
+    mode?: 'replace' | 'manual',
+    //  自动替换的文件
+    replace?: Array<string>,
+    //  手动替换的文件
+    manual?: Array<string>,
+    lockPath?: String,
+    // 插件名称
+    name?: String,
+    // 手动对比出错的回调
+    manualCallback?: (path: string, lock: Buffer) => void | never,
+    // 自动对比出错的回调
+    replaceCallback?: (path: string, lock: Buffer) => void | never,
+}
+type TRequireConfig = Required<Tconfig>
+interface IStirngMap {
+    [p: string]: string
+}
+export default function CreateLockCode(setting: Tconfig | undefined) {
+    const config = [formatSetting, formatPathMap].reduce((per, cur) => cur(per), setting) as TRequireConfig
     class LockCode {
-        apply(compiler) {
+        apply(compiler: any) {
             compiler.hooks.emit.callAsync(config.name, async () => {
                 return check(config)
             })
@@ -11,7 +30,7 @@ export default  function CreateLockCode(setting) {
     }
     return LockCode
 }
-function formatSetting(setting) {
+function formatSetting(setting: Tconfig | undefined): TRequireConfig {
     const defaultConfig = {
         mode: 'replace', // 'replace' 取代被篡改的代码 'manual' 仅报错需要手动替换
         replace: [],
@@ -23,20 +42,20 @@ function formatSetting(setting) {
         },
         replaceCallback(filePath, lock) {
             fs.writeFile(filePath, lock)
-            console.warn(filePath,'已经自动修复了')
+            console.warn(filePath, '已经自动修复了')
             return filePath
         }
-    }
+    } as TRequireConfig
     // 如果是数组写法则修正为 defaultConfig.mode
     if (Array.isArray(setting)) {
         return { ...defaultConfig, [defaultConfig.mode]: setting }
     }
     return Object.assign(defaultConfig, setting)
 }
-function formatPathMap(config) {
+function formatPathMap(config: TRequireConfig) {
     config.lockPath = path.resolve('', config.lockPath)
-    const pathMap = {}
-    function eachMode(mode) {
+    const pathMap: IStirngMap = {}
+    function eachMode(mode: TRequireConfig['mode']) {
         config[mode].forEach((string, index) => {
             const localPath = path.resolve('', string)
             pathMap[localPath] = mode
@@ -47,7 +66,7 @@ function formatPathMap(config) {
     eachMode('manual')
     return Object.assign(config, pathMap)
 }
-function check(config) {
+function check(config: TRequireConfig) {
     return new Promise(async (resolve, reject) => {
         const replaceFile = createFileHandle('replace', config.replaceCallback)
         const manualFile = createFileHandle('manual', config.manualCallback)
@@ -56,9 +75,9 @@ function check(config) {
         Promise.all([replacePromise, manualPromise]).then(resolve).catch(reject)
     })
 }
-function createFileHandle(mode, callback) {
-    return async function replaceFile(config) {
-        return config[mode].map((filePath) => {
+function createFileHandle(mode: TRequireConfig['mode'], callback: TRequireConfig['manualCallback']) {
+    return async function replaceFile(config: TRequireConfig) {
+        return config[mode].map((filePath: string) => {
             return new Promise(async (resolve, reject) => {
                 const lockFilePath = path.resolve(config.lockPath, filePath)
                 const localPath = path.resolve('', filePath)
@@ -68,14 +87,14 @@ function createFileHandle(mode, callback) {
                     console.log('lockFilePath', lockFilePath);
                     const [lock, src] = await Promise.all([fs.readFile(lockFilePath), fs.readFile(localPath)])
                     if (lock.toString() === src.toString()) {
-                        return resolve()
+                        return resolve(undefined)
                     }
                     // 不相等则直接覆盖
                     return callback(filePath, lock)
                     // fs.writeFile(filePath, lock)
                 } catch (error) {
                     // 不存在文件则进行记录
-                    fs.readFile(localPath).then(r => {
+                    fs.readFile(localPath).then((r: Buffer) => {
                         fs.outputFile(lockFilePath, r)
                     })
                 }
